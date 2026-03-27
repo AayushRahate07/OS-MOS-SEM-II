06091f60a73fec67c1eab6566ec013967d6edbf7
Aayush Rahate <rahateaayush50@gmail.com>
2026-03-27 11:05:18 +0530

    PHASE I MOS

---
+++ Phase1.cpp
@@ -0,0 +1,196 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class OS {
+private:
+    char M[100][4];   // Memory
+    char IR[4];       // Instruction Register
+    char R[4];        // General Register
+    int IC;           // Instruction Counter
+    int SI;           // Service Interrupt
+    bool C;           // Toggle flag
+
+    ifstream fin;
+    ofstream fout;
+    string line;
+
+public:
+
+    // 🔹 Initialize system
+    void init() {
+        for(int i=0;i<100;i++)
+            for(int j=0;j<4;j++)
+                M[i][j] = ' ';
+
+        IC = 0;
+        SI = 0;
+        C = false;
+
+        for(int i=0;i<4;i++) {
+            IR[i] = ' ';
+            R[i] = ' ';
+        }
+    }
+
+    // 🔹 Convert operand to address
+    int getAddr() {
+        return (IR[2]-'0')*10 + (IR[3]-'0');
+    }
+
+    // 🔹 MOS (Master Mode)
+    void MOS() {
+        int addr = getAddr();
+
+        if(SI == 1) { // READ
+            getline(fin, line);
+            int k = 0;
+
+            for(int i=0;i<10;i++) {
+                for(int j=0;j<4;j++) {
+                    if(k < line.size())
+                        M[addr][j] = line[k++];
+                    else
+                        M[addr][j] = ' ';
+                }
+                addr++;
+            }
+        }
+
+        else if(SI == 2) { // WRITE
+            for(int i=0;i<10;i++) {
+                for(int j=0;j<4;j++)
+                    fout << M[addr][j];
+                addr++;
+            }
+            fout << endl;
+        }
+
+        else if(SI == 3) { // HALT
+            fout << "\nJOB TERMINATED\n\n";
+        }
+
+        SI = 0;
+    }
+
+    // 🔹 Execute user program (Slave Mode)
+    void EXECUTE() {
+        while(true) {
+
+            // Fetch
+            for(int i=0;i<4;i++)
+                IR[i] = M[IC][i];
+            IC++;
+
+            // Decode
+            if(IR[0]=='G' && IR[1]=='D') {
+                SI = 1;
+                MOS();
+            }
+
+            else if(IR[0]=='P' && IR[1]=='D') {
+                SI = 2;
+                MOS();
+            }
+
+            else if(IR[0]=='H') {
+                SI = 3;
+                MOS();
+                break;
+            }
+
+            else if(IR[0]=='L' && IR[1]=='R') {
+                int addr = getAddr();
+                for(int i=0;i<4;i++)
+                    R[i] = M[addr][i];
+            }
+
+            else if(IR[0]=='S' && IR[1]=='R') {
+                int addr = getAddr();
+                for(int i=0;i<4;i++)
+                    M[addr][i] = R[i];
+            }
+
+            else if(IR[0]=='C' && IR[1]=='R') {
+                int addr = getAddr();
+                C = true;
+                for(int i=0;i<4;i++)
+                    if(R[i] != M[addr][i])
+                        C = false;
+            }
+
+            else if(IR[0]=='B' && IR[1]=='T') {
+                int addr = getAddr();
+                if(C)
+                    IC = addr;
+            }
+        }
+    }
+
+    // 🔹 Load program
+    void LOAD() {
+        fin.open("input.txt");
+        fout.open("output.txt");
+
+        if(!fin) {
+            cout << "Error opening input file\n";
+            return;
+        }
+
+        int mem_ptr = 0;
+
+        while(getline(fin, line)) {
+
+            if(line.find("$AMJ") != string::npos) {
+                init();
+                mem_ptr = 0;
+            }
+
+            else if(line.find("$DTA") != string::npos) {
+                EXECUTE();
+            }
+
+            else if(line.find("$END") != string::npos) {
+                printMemory();
+                fout << "-----------------------------\n";
+            }
+
+            else {
+                int k = 0;
+                while(k < line.size()) {
+                    for(int j=0;j<4;j++) {
+                        if(k < line.size())
+                            M[mem_ptr][j] = line[k++];
+                        else
+                            M[mem_ptr][j] = ' ';
+                    }
+                    mem_ptr++;
+                }
+            }
+        }
+
+        fin.close();
+        fout.close();
+    }
+
+    // 🔹 Print memory (for debugging/viva)
+    void printMemory() {
+        cout << "\n--- MEMORY DUMP ---\n";
+        for(int i=0;i<100;i++) {
+            cout << "M[" << i << "] : ";
+            for(int j=0;j<4;j++)
+                cout << M[i][j];
+            cout << endl;
+
+            if(i % 10 == 9) cout << endl;
+        }
+    }
+};
+
+int main() {
+    OS os;
+    os.LOAD();
+    cout << "Execution Complete. Check output.txt\n";
+    return 0;
+}
